d42639273caeb11bf2ff9ab184df51369e912e91
ssakuragawa <ssakuragawa@crooz.co.jp>
2013-09-26 17:05:39 +0900

    test用のhtmlを作成した。

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"	
+"Http://www.w3.org/TR/xhtml1/DVD/xhtml1-transitional.dtd">
+<html>
+<head>
+ <meta http-equiv="Contrnt-type" content="text/html; charset=utf-8"/>
+ <title>hello,world</title>
+</head>
+<body>
+  <p>hello,world</p>
+</body>
+</html>
