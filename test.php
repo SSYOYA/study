c088ee822a14ab5f3ba32bc49dc557fadfa45100
ssakuragawa <ssakuragawa@crooz.co.jp>
2013-09-26 17:39:58 +0900

    時間により挨拶をするように変更。

--- test.php
+++ test.php
@@ -3,9 +3,20 @@
 <html>
 <head>
  <meta http-equiv="Contrnt-type" content="text/html; charset=utf-8"/>
- <title>hello,world</title>
+ <title>あいさつ</title>
 </head>
 <body>
-  <p>hello,world</p>
-</body>
+  <?php  $hour = date('H'); // 現在の時を$hour変数に格納 ?>
+
+  <?php if (5 <= $hour && $hour < 10): // ５時から１０時までの間 ?>
+  <P>おはようございます</P>
+  <?php elseif (10 <= $hour && $hour < 18): // １０時から１８時までの間 ?>
+  <p>こんにちは。</p>
+  <?php else: // それ以外 ?>
+  <P>こんばんは。</P>
+  <?php endif; ?>
+  
+  <p>現在<?php echo $hour; ?>時です。</p>
+  
+  </body>
 </html>
